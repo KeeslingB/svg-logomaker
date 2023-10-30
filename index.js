4541a7b636e79bf4fea4dc45f1aa805b0a03c9f8
unknown <Breke64@gmail.com>
2023-10-29 23:00:19 -0400

    worked on homework more still having trouble getting users answer corresponding shape to generate

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { Circle, Triangle, Square } = require('./lib/shapes');
+const { Circle, Triangle, Square } = require('./lib/shapes.js');
 
+// module.imports = {Triangle, Circle, Square};
 
 inquirer.prompt([
   {
@@ -33,28 +34,30 @@ createPage(response));
 
   
 function createPage(response) {
+  svgfile = `<svg version="1.1"
+  width="300" height="200"
+  xmlns="http://www.w3.org/2000/svg">
+
+  
+  ${shapeChoice.response}
+  
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textcolor}">${response.text}</text>
+
+</svg>`;
   fs.writeFile('logo.svg', svgfile, (err) =>
-  err ? console.error(err) : console.log('success'));
+  err ? console.error(err) : console.log('Generated logo.svg'));
 }
 
 
-//file made after prompts named logo.svg
-//output text 'generated logo.svg 
-//svg images 300x200 pixel size
 
-/*
-WHEN I am prompted for text
-THEN I can enter up to three characters
-WHEN I am prompted for the text color
-THEN I can enter a color keyword (OR a hexadecimal number)
-WHEN I am prompted for a shape
-THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-WHEN I am prompted for the shape's color
-THEN I can enter a color keyword (OR a hexadecimal number)
-WHEN I have entered input for all the prompts
-THEN an SVG file is created named `logo.svg`
-AND the output text "Generated logo.svg" is printed in the command line
-WHEN I open the `logo.svg` file in a browser
-THEN I am shown a 300x200 pixel image that matches the criteria I entered
-*/
-
+function shapeChoice (shapes) {
+  if (shapes === 'Triangle') {
+    return Triangle;
+  }
+  if (shapes === 'Square') {
+    return Square;
+  }
+  else {
+    return Circle;
+  }
+}
